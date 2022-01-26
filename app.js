f086c4e219b5faa030dbf3d4ffb13e2219f50b91
Ruben Rodrigues <ruben2000@gmail.com>
2022-01-26 09:14:34 +0000

    Pre-apresentação

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ new Vue({
       this.$http.get('/api/events')
         .success(function (events) {
           this.$set('events', events);
-          console.log("TESTE");
+          console.log("TESTE APRESENTAÇÃO");
           console.log(events);
         })
         .error(function (err) {
